dbdb44f3d3d6a91599a4779ba251d7e14671b2f8
kcTest <763336180@qq.com>
2022-02-24 19:50:31 +0800

    -brand

--- src/api/brand.js
+++ src/api/brand.js
@@ -1,5 +1,31 @@
 import request from "@/utils/request";
 
+export function deleteBrand(id) {
+    return request({
+        url: '/brand/delete/' + id,
+        method: 'get',
+    });
+}
+
+
+export function updateShowStatus(data) {
+    return request({
+        url: '/brand/update/showStatus',
+        method: 'post',
+        data: data
+    });
+}
+
+
+export function updateFactoryStatus(data) {
+    return request({
+        url: '/brand/update/factoryStatus',
+        method: 'post',
+        data: data
+    });
+}
+
+
 export function fetchList(params) {
     return request({
         url: '/brand/list',
