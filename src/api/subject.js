c367d1a73d1bd222dd1188bb5fc2bfea31dff76a
kcTest <763336180@qq.com>
2022-03-01 16:53:44 +0800

    -fix

--- src/api/subject.js
+++ src/api/subject.js
@@ -1,10 +1,17 @@
 import request from "@/utils/request";
 
+export function fetchListAll() {
+    return request({
+        url: '/subject/listAll',
+        method: 'get',
+    })
+}
+
 
 export function fetchList(params) {
     return request({
-        url:'/subject/list',
-        method:'get',
-        params:params
+        url: '/subject/list',
+        method: 'get',
+        params: params
     })
 }
